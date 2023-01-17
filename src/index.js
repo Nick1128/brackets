c132c4011de638be1e81c47f72cd2c92d0e156a5
Nick1128 <bubeshko@gmail.com>
2023-01-17 14:14:58 +0100

    code:add new code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.map(item => item.join(''));
+
+  while (bracketsConfig.length) {
+    let counter = 0;
+
+    bracketsConfig.forEach(item => {
+      if (str.indexOf(item) != -1) {
+        str = str.replace(item, '');
+      } else {
+        counter += 1;
+      }
+    });
+
+    if (counter === bracketsConfig.length) {
+      bracketsConfig.length = 0;
+    }
+  }
+  return str.length === 0 ? true : false;
 }
